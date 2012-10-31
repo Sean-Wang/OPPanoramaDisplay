ead3e04f9a19dfebe0f5529e3892310817e29b7f
QA Team <admin@admintekiMac-mini.local>
2012-10-31 13:36:11 +0800

    Initial Commit

---
+++ OPPanoramaDisplay/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  OPPanoramaDisplay
+//
+//  Created by QA Team on 12-10-31.
+//  Copyright (c) 2012年 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ OPPanoramaDisplay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OPPanoramaDisplay
+//
+//  Created by QA Team on 12-10-31.
+//  Copyright (c) 2012年 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
